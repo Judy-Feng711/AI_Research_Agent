32510034163d2d0b43534fc9af7fd56434496f22
Judy-Feng711 <fengxiuling-001@163.com>
2026-09-02 17:00:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -202,7 +202,7 @@ st.markdown(
         }
 
         [data-testid="stHorizontalBlock"] {
-            gap: 0 !important;
+            gap: 10 !important;
         }
         [data-testid="stHorizontalBlock"] .stColumn {
             border-left: none !important;
