8f9bcb4974744152b77b0d001dfc2dc8cbcd7d7f
Judy-Feng711 <fengxiuling-001@163.com>
2026-09-03 11:27:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -483,7 +483,7 @@ st.markdown(
             transform: translate(-50%, -50%);
         }
         /* 隐藏 text_area 右下角的快捷键提示 */
-        .stTextArea > div:last-child {
+        .stTextArea [data-testid="stTextArea"] + div {
             display: none !important;
         }
     </style>
